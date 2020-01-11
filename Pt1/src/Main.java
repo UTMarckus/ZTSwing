8623547a42cf6b2f59810408f3777e2ffc0489bb
'UTMarckus' <'utmarckus@gmail.com'>
2020-01-11 16:46:42 +0200

    v0.2

--- Pt1/src/Main.java
+++ Pt1/src/Main.java
@@ -1,9 +1,20 @@
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Window window = new Window();
 		window.setVisible(true);
+		window.setSize(450, 350);
+		window.setLayout(null);
+		JScrollPane scrPane = new JScrollPane();
+		scrPane.setBounds(25, 25, 350, 250);
+		window.add(scrPane);
+		JTextArea txtArea = new JTextArea();
+		txtArea.setText("asdsadsadsadsafddsgfdsokv,ejtb,moiosjtvbmildsuthgjkesrvhgnksuerhgvjkesrhgnsjkervgn");
+		scrPane.setViewportView(txtArea);
 
 	}
 
