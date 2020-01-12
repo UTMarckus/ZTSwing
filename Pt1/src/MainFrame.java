7bc8f36eb9c9ff09529a4971fe39963a6ef68d16
UTMarckus <utmarckus@gmail.com>
2020-01-12 10:53:12 +0200

    v0.15

---
+++ Pt1/src/MainFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+	
+	public MainFrame() {
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setSize(400, 300);
+		
+	}
+
+}
