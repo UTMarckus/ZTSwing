f7a920ef545eebc84345b52427073d963c89365a
UTMarckus <utmarckus@gmail.com>
2020-01-13 12:04:49 +0200

    v0.3

--- Pt2/src/ua/utm/pt2/MainWindow.java
+++ Pt2/src/ua/utm/pt2/MainWindow.java
@@ -7,11 +7,29 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 
 public class MainWindow extends JFrame {
+
+	private JMenuItem menuItemExit;
+	private JMenu menuOptions;
+	private JMenuItem menuItemSaveAs;
+	private JMenu menuFile;
+	private JMenuBar menuBar;
+	private JButton btnView;
+	private JLabel lblFormat;
+	private Choice format;
+	private JButton btnGetFile;
+	private JButton btnGetImage;
+	private JTextArea txtEnterUrlHere;
+	private JTabbedPane tabbedPane;
+	private JPanel panel2;
+	private JPanel panel1;
 
 	public MainWindow(int width, int height) {
 		setSize(width, height);
@@ -19,23 +37,22 @@ public class MainWindow extends JFrame {
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(null);
 		
-		JPanel panel1 = new JPanel(null);
-		JPanel panel2 = new JPanel(null);
+		panel1 = new JPanel(null);
+		panel2 = new JPanel(null);
 		
-		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(10, 40, 605, 392);
 		getContentPane().add(tabbedPane);
-		setVisible(true);
 		setResizable(false);
 		
 		tabbedPane.addTab("Tab1", panel1);
 		
-		JTextArea  txtEnterUrlHere = new JTextArea();
+		txtEnterUrlHere = new JTextArea();
 		txtEnterUrlHere.setText("Enter URL here...");
 		txtEnterUrlHere.setBounds(10, 40, 580, 313);
 		panel1.add(txtEnterUrlHere);
 		
-		JButton btnGetImage = new JButton("Get Image");
+		btnGetImage = new JButton("Get Image");
 		btnGetImage.setBounds(10, 10, 140, 20);
 		//btnGetImage.addActionListener(e -> System.out.println("1"));
 		btnGetImage.addActionListener(new ActionListener() {
@@ -46,28 +63,45 @@ public class MainWindow extends JFrame {
 		});
 		panel1.add(btnGetImage);
 		
-		JButton btnGetFile = new JButton("Get File");
+		btnGetFile = new JButton("Get File");
 		btnGetFile.setBounds(450, 10, 140, 20);
 		btnGetFile.addActionListener(e -> System.out.println("2"));
 		panel1.add(btnGetFile);
 		
 		tabbedPane.addTab("Tab2", panel2);
 		
-		Choice format = new Choice();
+		format = new Choice();
 		format.setBounds(520, 10, 70, 14);
 		format.add("*.png");
 		format.add("*.jpg, *.jpeg");
 		panel2.add(format);
 		
-		JLabel lblFormat = new JLabel("Format:");
+		lblFormat = new JLabel("Format:");
 		lblFormat.setBounds(470, 13, 50, 14);
 		panel2.add(lblFormat);
 		
-		JButton btnView = new JButton("View");
+		btnView = new JButton("View");
 		btnView.setBounds(10, 10, 140, 20);
 		btnView.addActionListener(e -> System.out.println("3"));
 		panel2.add(btnView);
 		
+		menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 640, 20);
+		getContentPane().add(menuBar);
+		
+		menuFile = new JMenu("File");
+		menuBar.add(menuFile);
+		
+		menuItemSaveAs = new JMenuItem("Save as...");
+		menuFile.add(menuItemSaveAs);
+		
+		menuOptions = new JMenu("Options");
+		menuBar.add(menuOptions);
+		
+		menuItemExit = new JMenuItem("Exit");
+		menuOptions.add(menuItemExit);
+		
+		setVisible(true);
 	}
 
 }
